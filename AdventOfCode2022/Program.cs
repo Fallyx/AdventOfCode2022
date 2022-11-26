e764e7170c20885c43706a6609577fbde534c542
Janis <Janis@DESKTOP-H1S72A1>
2022-11-26 14:19:12 +0100

    Projektdateien hinzufügen.

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode2022;
+
+class Program
+{
+    private static Stopwatch swDay = new Stopwatch();
+
+    static void Main(string[] args)
+    {
+        string? input;
+        if (args.Length != 0)
+        {
+            input = args[0];
+        }
+        else
+        {
+            Console.WriteLine("Run a day [1-25]:");
+            input = Console.ReadLine();
+            Console.WriteLine("-----------------\n");
+        }
+
+        bool success = int.TryParse(input, out int day);
+        if (!success)
+        {
+            return;
+        }
+
+        SingleRun(day);    
+    }
+
+    private static void SingleRun(int day)
+    {
+        swDay.Start();
+
+        switch (day)
+        {
+            default:
+                break;
+        }
+
+        swDay.Stop();
+        Console.WriteLine($"Day {day:D2} elapsed time: {swDay.Elapsed}\n");
+    }
+}
