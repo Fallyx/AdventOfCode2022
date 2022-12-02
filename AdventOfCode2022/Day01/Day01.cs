b02000523de68f274d708741d0a661d1a977fa5f
Fallyx <Fallyx@users.noreply.github.com>
2022-12-02 11:47:50 +0100

    Solved Day 02

--- AdventOfCode2022/Day01/Day01.cs
+++ AdventOfCode2022/Day01/Day01.cs
@@ -1,40 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace AdventOfCode2022.Day01;
 
-namespace AdventOfCode2022.Day01
+internal class Day01
 {
-    internal class Day01
+    const string inputPath = @"Day01/Input.txt";
+
+    public static void Task1and2()
     {
-        const string inputPath = @"Day01/Input.txt";
+        List<String> lines = File.ReadAllLines(inputPath).ToList();
 
-        public static void Task1and2()
+        List<int> calories = new List<int>();
+
+        int currentCalories = 0;
+        foreach (String line in lines)
         {
-            List<String> lines = File.ReadAllLines(inputPath).ToList();
-
-            List<int> calories = new List<int>();
-
-            int currentCalories = 0;
-            foreach (String line in lines)
+            if (line.Length == 0)
             {
-                if (line.Length == 0)
-                {
-                    calories.Add(currentCalories);
-                    currentCalories = 0;
-                    continue;
-                }
-
-                currentCalories += int.Parse(line);
+                calories.Add(currentCalories);
+                currentCalories = 0;
+                continue;
             }
 
-            calories.Add(currentCalories);
-
-            calories.Sort();
-            calories.Reverse();
-            Console.WriteLine($"Task 1: {calories.First()}");
-            Console.WriteLine($"Task 2: {calories.Take(3).Sum()}");
+            currentCalories += int.Parse(line);
         }
+
+        calories.Add(currentCalories);
+
+        calories.Sort();
+        calories.Reverse();
+        Console.WriteLine($"Task 1: {calories.First()}");
+        Console.WriteLine($"Task 2: {calories.Take(3).Sum()}");
     }
 }
---
+++ AdventOfCode2022/Day02/Day02.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2022.Day02;
+internal class Day02
+{
+    const string inputPath = @"Day02/Input.txt";
+
+    public static void Task1and2()
+    {
+        List<String> lines = File.ReadAllLines(inputPath).ToList();
+        int pointsTask1 = 0;
+        int pointsTask2 = 0;
+
+        foreach (String line in lines)
+        {
+            String rpc = line.Replace(" ", "");
+
+            pointsTask1 += PlayTask1(rpc);
+            pointsTask2 += PlayTask2(rpc);
+        }
+
+        Console.WriteLine($"Task 1: {pointsTask1}");
+        Console.WriteLine($"Task 2: {pointsTask2}");
+    }
+
+    private static int PlayTask1(string rpc)
+    {
+        int score = 0;
+        if (rpc[1] == 'X') score += 1;
+        else if (rpc[1] == 'Y') score += 2;
+        else if (rpc[1] == 'Z') score += 3;
+
+        if (rpc == "AX" || rpc == "BY" || rpc == "CZ") score += 3;
+        else if (rpc == "CX" || rpc == "AY" || rpc == "BZ") score += 6;
+
+        return score;
+    }
+
+    private static int PlayTask2(string rpc)
+    {
+        int score = 0;
+
+        if (rpc == "AX") score += 3;
+        else if (rpc == "AY") score += 1 + 3;
+        else if (rpc == "AZ") score += 2 + 6;
+        else if (rpc == "BX") score += 1;
+        else if (rpc == "BY") score += 2 + 3;
+        else if (rpc == "BZ") score += 3 + 6;
+        else if (rpc == "CX") score += 2;
+        else if (rpc == "CY") score += 3 + 3;
+        else if (rpc == "CZ") score += 1 + 6;
+
+        return score;
+    }
+}
