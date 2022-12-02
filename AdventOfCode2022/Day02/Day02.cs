67e9be8c5849e5312b3bf85db8be6ddad09e1922
Fallyx <Fallyx@users.noreply.github.com>
2022-12-02 22:00:28 +0100

    Updated Day 2 part 2

--- AdventOfCode2022/Day02/Day02.cs
+++ AdventOfCode2022/Day02/Day02.cs
@@ -2,12 +2,17 @@
 internal class Day02
 {
     const string inputPath = @"Day02/Input.txt";
+    private static Dictionary<string, int> outcomesPart2 = new Dictionary<string, int>()
+    {
+        { "AX", 3 }, { "AY", 1 + 3 }, { "AZ", 2 + 6 }, { "BX", 1 }, { "BY", 2 + 3 }, { "BZ", 3 + 6 }, { "CX", 2 }, { "CY", 3 + 3 }, { "CZ", 1 + 6 }
+    };
 
     public static void Task1and2()
     {
         List<String> lines = File.ReadAllLines(inputPath).ToList();
         int pointsTask1 = 0;
         int pointsTask2 = 0;
+        int pointsTask3 = 0;
 
         foreach (String line in lines)
         {
@@ -19,6 +24,7 @@ internal class Day02
 
         Console.WriteLine($"Task 1: {pointsTask1}");
         Console.WriteLine($"Task 2: {pointsTask2}");
+        Console.WriteLine($"Task 2: {pointsTask3}");
     }
 
     private static int PlayTask1(string rpc)
@@ -36,18 +42,6 @@ internal class Day02
 
     private static int PlayTask2(string rpc)
     {
-        int score = 0;
-
-        if (rpc == "AX") score += 3;
-        else if (rpc == "AY") score += 1 + 3;
-        else if (rpc == "AZ") score += 2 + 6;
-        else if (rpc == "BX") score += 1;
-        else if (rpc == "BY") score += 2 + 3;
-        else if (rpc == "BZ") score += 3 + 6;
-        else if (rpc == "CX") score += 2;
-        else if (rpc == "CY") score += 3 + 3;
-        else if (rpc == "CZ") score += 1 + 6;
-
-        return score;
+        return outcomesPart2[rpc];
     }
 }
