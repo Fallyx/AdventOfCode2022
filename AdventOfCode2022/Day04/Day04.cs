6b5db6b8029a7e13b27d6120a574ac9d721a0b9f
Fallyx <Fallyx@users.noreply.github.com>
2022-12-04 11:02:09 +0100

    Refactored Day 04

--- AdventOfCode2022/Day04/Day04.cs
+++ AdventOfCode2022/Day04/Day04.cs
@@ -12,13 +12,10 @@ internal class Day04
 
         foreach (string pair in lines)
         {
-            string[] pairs = pair.Split(',');
+            int[] ids = pair.Split(new char[] { ',', '-' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
-            int[] ids1 = pairs[0].Split('-').Select(int.Parse).ToArray();
-            int[] ids2 = pairs[1].Split('-').Select(int.Parse).ToArray();
-
-            if ((ids1[0] <= ids2[0] && ids1[1] >= ids2[1]) || (ids2[0] <= ids1[0] && ids2[1] >= ids1[1])) fullyContains++;
-            int overlapsCount = Math.Max(0, Math.Min(ids1[1], ids2[1]) - Math.Max(ids1[0], ids2[0]) + 1);
+            if ((ids[0] <= ids[2] && ids[1] >= ids[3]) || (ids[2] <= ids[0] && ids[3] >= ids[1])) fullyContains++;
+            int overlapsCount = Math.Max(0, Math.Min(ids[1], ids[3]) - Math.Max(ids[0], ids[2]) + 1);
             overlaps += (overlapsCount > 0 ? 1 : 0 );
         }
 
