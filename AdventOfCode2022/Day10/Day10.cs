ae2d37db6331e332f26221eb2932e812a1785d5f
Fallyx <Fallyx@users.noreply.github.com>
2022-12-10 12:45:54 +0100

    Solved Day 10

---
+++ AdventOfCode2022/Day10/Day10.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode2022.Day10;
+
+internal class Day10
+{
+    const string inputPath = @"Day10/Input.txt";
+    public static void Task1and2()
+    {
+        List<String> lines = File.ReadAllLines(inputPath).ToList();
+        int maxCycle = 240;
+        List<int> signalStrengthCycles = new List<int> { 20, 60, 100, 140, 180, 220 };
+        int linesIdx = 0;
+        bool firstCycle = true;
+        int X = 1;
+        int signalStrength = 0;
+
+        Console.WriteLine("Task 2:");
+
+        for (int i = 1; i <= maxCycle; i++)
+        {
+            if (signalStrengthCycles.Contains(i))
+                signalStrength += i * X;
+
+            int pixel = (i - 1) % 40;
+            if ( (X - 1) <= pixel && pixel <= (X + 1) )
+                Console.Write("#");
+            else
+                Console.Write(".");
+
+            if (i % 40 == 0)
+                Console.WriteLine();
+
+            if (firstCycle && lines[linesIdx].StartsWith("addx"))
+            {
+                firstCycle = false;
+                continue;
+            }
+            else if (!firstCycle && lines[linesIdx].StartsWith("addx"))
+            {
+                firstCycle = true;
+                X += int.Parse(lines[linesIdx].Split(' ')[1]);
+            }
+
+            linesIdx++;
+        }
+
+        Console.WriteLine($"\nTask 1: {signalStrength}");
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -63,6 +63,9 @@ class Program
             case 9:
                 Day09.Day09.Task1and2();
                 break;
+            case 10:
+                Day10.Day10.Task1and2();
+                break;
             default:
                 break;
         }
